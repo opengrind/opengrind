b554721e57bed557d86e03ddfce8aec25f93cb54
Regis Freyd <djaiss@users.noreply.github.com>
2023-05-24 07:42:45 -0400

    wip

--- resources/views/profile/settings/partials/livewire-update-user-date-of-birth.blade.php
+++ resources/views/profile/settings/partials/livewire-update-user-date-of-birth.blade.php
@@ -14,15 +14,15 @@
       <div class="space-y-2 mb-4">
         <div class="flex items-center gap-x-2">
           <input id="hidden" wire:model.defer="agePreferences" value="hidden" name="date-birth" type="radio" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
-          <label for="hidden" class="block text-sm font-medium leading-6 text-gray-900">Never display the date of birth to anyone</label>
+          <label for="hidden" class="block text-sm font-medium leading-6 text-gray-900">{{ __('Never display the date of birth to anyone') }}</label>
         </div>
         <div class="flex items-center gap-x-2">
           <input id="month_day" wire:model.defer="agePreferences" value="month_day" name="date-birth" type="radio" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
-          <label for="month_day" class="block text-sm font-medium leading-6 text-gray-900">Only show the day and the month</label>
+          <label for="month_day" class="block text-sm font-medium leading-6 text-gray-900">{{ __('Only show the day and the month') }}</label>
         </div>
         <div class="flex items-center gap-x-2">
           <input id="full" wire:model.defer="agePreferences" value="full" name="date-birth" type="radio" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
-          <label for="full" class="block text-sm font-medium leading-6 text-gray-900">Display the full date of birth</label>
+          <label for="full" class="block text-sm font-medium leading-6 text-gray-900">{{ __('Display the full date of birth') }}</label>
         </div>
       </div>
 
